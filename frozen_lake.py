1147db067c6f37ee25ff986c0015e84d96b9a667
SimonH <professional.simonhossain@gmail.com>
2025-06-17 22:53:49 +0100

    q-learning formula

--- frozen_lake.py
+++ frozen_lake.py
@@ -1,9 +1,14 @@
 import gymnasium as gym
 from gymnasium.envs.toy_text.frozen_lake import generate_random_map
+import numpy as np
 
 
 def run_FL():
     env = gym.make('FrozenLake-v1', desc=generate_random_map(size=4), map_name="4x4", is_slippery=False, render_mode='human')
+
+    q = np.zeros((env.observation_space.n, env.action_space.n)) # Q-table for the agent
+    learning_rate = 0.9
+    discount_factor = 0.9
 
     observation, info = env.reset()
 
