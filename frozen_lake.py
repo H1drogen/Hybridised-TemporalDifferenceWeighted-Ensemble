544becf7338ebbf3a12f5ac3f7302df7eb8d33f4
SimonH <professional.simonhossain@gmail.com>
2025-06-17 22:49:10 +0100

    creating a function

--- frozen_lake.py
+++ frozen_lake.py
@@ -2,21 +2,21 @@ import gymnasium as gym
 from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 
 
-# Initialise the environment
-env = gym.make('FrozenLake-v1', desc=generate_random_map(size=4), map_name="4x4", is_slippery=False, render_mode='human')
+def run_FL():
+    env = gym.make('FrozenLake-v1', desc=generate_random_map(size=4), map_name="4x4", is_slippery=False, render_mode='human')
 
-observation, info = env.reset()
+    observation, info = env.reset()
 
-env.render()
+    env.render()
 
-episode_over = False
-while not episode_over:
-    action = env.action_space.sample()  # agent policy that uses the observation and info
-    observation, reward, terminated, truncated, info = env.step(action)
-    print(f'observation tile: {observation}')
+    episode_over = False
+    while not episode_over:
+        action = env.action_space.sample()  # agent policy that uses the observation and info
+        observation, reward, terminated, truncated, info = env.step(action)
+        print(f'observation tile: {observation}')
 
-    episode_over = terminated or truncated
+        episode_over = terminated or truncated
 
-print('final reward:', reward)
+    print('final reward:', reward)
 
-env.close()
+    env.close()
