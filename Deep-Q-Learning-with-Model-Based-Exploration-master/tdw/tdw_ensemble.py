bd778eb96468ec40d9936f4a895ac3bf9cccc15d
SimonH <professional.simonhossain@gmail.com>
2025-08-16 20:04:17 +0100

    Refactor act method to track agent contributions and return the most contributing agent

--- Deep-Q-Learning-with-Model-Based-Exploration-master/tdw/tdw_ensemble.py
+++ Deep-Q-Learning-with-Model-Based-Exploration-master/tdw/tdw_ensemble.py
@@ -68,16 +68,26 @@ class TDWVoteEnsemble(AccumulateErrorEnsemble):
 
     def act(self, obs):
         self._update_prev_qs(obs)
+        # check self.prev_qs
 
         weights = self._get_weights()
+        print("weights:", weights)
 
         if self.visualizer is not None:
             self.visualizer.update(weights)
 
         votes = np.zeros(len(self.prev_qs[0]), dtype=np.float32)
-        for w, q in zip(weights, self.prev_qs):
-            votes[np.argmax(q)] += w
+        # for w, q in zip(weights, self.prev_qs):
+        #     votes[np.argmax(q)] += w
+
+        agent_contributions = np.zeros(len(self.prev_qs), dtype=np.float32)
+        for i, (w, q) in enumerate(zip(weights, self.prev_qs)):
+            action_index = np.argmax(q)
+            votes[action_index] += w
+            agent_contributions[i] += w if action_index == np.argmax(votes) else 0
 
         action = np.argmax(votes)
+        most_contributing_agent = np.argmax(agent_contributions)
+
         self.prev_action = action
-        return action
+        return action, most_contributing_agent
